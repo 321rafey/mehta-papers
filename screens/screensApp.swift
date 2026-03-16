855408d55609a75c562b960b86f6eeb5115d529c
rafey <rafeykashan@gmail.com>
2026-03-17 02:15:45 +0500

    Initial Commit

---
+++ screens/screensApp.swift
@@ -0,0 +1,17 @@
+//
+//  screensApp.swift
+//  screens
+//
+//  Created by Rafey Kashan on 17/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct screensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
